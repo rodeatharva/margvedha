4d7a3951b1c36abd58577bcf9534f455b8a3431b
rodeatharva <116911507+rodeatharva@users.noreply.github.com>
2025-04-12 21:45:24 +0530

    Update settings.py

--- MargVedha/settings.py
+++ MargVedha/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'MargVedha.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Ensure this points to the correct path
     }
 }
 
